34ebfc259d873e631a629805071f16279cb80794
Viktor Fonic <viktor.fonic@gmail.com>
2018-05-22 20:17:56 +0200

    Add correct tokens

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "your_api_key"
-  config.secret = "your_app_secret"
+  config.api_key = "855e4c90eae421837106dd53fa1d633c"
+  config.secret = "6f38a8ba8a429c5a06f4dac94357cfcd"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
